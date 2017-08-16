a57de84c8020312607c777a260f3f65d117b2021
Thom Allen <thomwork@gmail.com>
2017-08-16 11:57:34 +0100

    Add Links

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const Home = () => <h1>Home</h1>
+const Links = () => {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      {' '}
+      <Link to="/about">About</Link>
+    </div>  
+  )
+}
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Route exact path="/" component={Home} />
-          {/* <Route path="/about" render={() => <h1>about</h1>} /> */}
-          <Route
-            path="/about"
-            children={({ match }) => match && <h1>About</h1>}
-          />
-        </div>
-      </Router>
+      <div>
+        <Router>
+          <div>
+            <Route exact path="/" component={Home} />
+            {/* <Route path="/about" render={() => <h1>about</h1>} /> */}
+            <Route
+              path="/about"
+              children={({ match }) => match && <h1>About</h1>}
+            />
+            <Links />
+          </div>
+          
+        </Router>
+        
+      </div>
     );
   }
 }
