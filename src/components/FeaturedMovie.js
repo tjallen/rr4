52363b1f5a46beba7e4f1d0445532e5e8136ae71
Thom Allen <thomwork@gmail.com>
2017-08-18 12:29:23 +0100

    FeaturedMovie component content

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -1,3 +1,15 @@
-import React from 'react';
-const FeaturedMovie = () => (<p>FeaturedMovie</p>)
+import React from 'react'
+const FeaturedMovie = ({ movie }) => (
+  <div className="featured-movie">
+    <div className="featured-movie__image">
+      <img alt={movie.name} src={movie.image} />
+    </div>
+    
+    <div className="featured-movie__info">
+      <p><b>{movie.name}</b></p>
+      <p>{movie.director}</p>
+      <p>{movie.released}</p>
+    </div>
+  </div>
+)
 export default FeaturedMovie;
