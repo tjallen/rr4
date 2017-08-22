3b631ffdf2db40125151b17001369d1770f29974
Thom Allen <thomwork@gmail.com>
2017-08-22 16:25:07 +0100

    NavLink + activeClassName prop example

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
   BrowserRouter as Router,
   Route,
   Link,
+  NavLink,
   Switch
 } from 'react-router-dom';
 
@@ -19,8 +20,8 @@ class App extends Component {
         <div className="App">
           <div className="App-header">
             <h2>Movies app example</h2>
-            <Link to="/">Home</Link>
-            <Link to="/movies">Movies</Link>
+            <NavLink exact to="/" activeClassName='nav--active' >Home</NavLink>
+            <NavLink exact to="/movies" activeClassName='nav--active'>Movies</NavLink>
           </div>
 
           <div className="container">
